73ad58d78568cb13ee232e934c37513a638c1c0f
Sam-Gledhill <CodeZilla12@github.com>
2024-05-14 13:01:59 +0100

    Added csv reader function

--- graph_page.py
+++ graph_page.py
@@ -14,3 +14,30 @@ class GraphPage(tk.Frame):
         tk.Frame.__init__(self, parent)
 
         self.controller = controller
+
+    @ staticmethod
+    def get_words_and_scores_from_csv(file_path: str) -> list:
+
+        # This should be moved to an external utilities function as it will likely be used in many places.
+
+        # Items are stored in the csv as:
+        # word, trans, score[/n]word,trans,score[\n]....
+        word_list = []
+
+        if not isfile(file_path):
+            raise FileNotFoundError("word_scores.csv missing")
+
+        with open(file_path, 'r', encoding="utf8") as f:
+            csv_reader = csv.reader(f, delimiter=",")
+            for row in csv_reader:
+
+                if len(row) == 0:
+                    continue
+
+                if len(row) == 2:  # Make this check more specific
+                    row.append(0)
+
+                word, translated_word, score = row
+                word_list.append([word, translated_word, float(score)])
+
+        return word_list
