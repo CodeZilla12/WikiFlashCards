a55446b2e25ce0043614e5b901dc96c1f9d62bff
Sam-Gledhill <CodeZilla12@github.com>
2024-05-18 16:56:40 +0100

    Changed window title to include current file name

--- flashcard_viewer.py
+++ flashcard_viewer.py
@@ -24,7 +24,7 @@ class FlashcardViewer(tk.Toplevel):
         self.word_index = 0
 
         self.geometry("600x200")
-        self.title("Flashcard Viewer")
+        self.title(f"Editing {self.SELECTED_FLASHCARD_FILE.split('.')[0]}")
 
         # Iterate through each entry in selected config.
         entry_frame = tk.Frame(self)
