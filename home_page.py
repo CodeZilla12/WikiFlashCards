cd43ffdad977842d0ee3a7524510d369569833e1
Sam-Gledhill <CodeZilla12@github.com>
2024-05-18 17:50:35 +0100

    Can now make blank flashcard file - adds to optionlist

--- home_page.py
+++ home_page.py
@@ -24,15 +24,15 @@ class HomePage(tk.Frame):
 
         self.selected_file_tkstring = tk.StringVar(self)
 
-        flashcard_file_folder = self.CONFIG_OBJECT["Variables"]["flashcard-folder"]
+        self.flashcard_file_folder = self.CONFIG_OBJECT["Variables"]["flashcard-folder"]
         selected_flashcard_file_path = self.CONFIG_OBJECT["Variables"]["selected-flashcard-file"]
         self.selected_file_tkstring.set(selected_flashcard_file_path)
 
-        flashcard_file_list = listdir(flashcard_file_folder)
+        flashcard_file_list = listdir(self.flashcard_file_folder)
 
-        option_menu = tk.OptionMenu(
+        self.option_menu = tk.OptionMenu(
             self, self.selected_file_tkstring, *flashcard_file_list, command=self.on_new_flashcard_file_selected)
-        option_menu.grid(row=0, column=1)
+        self.option_menu.grid(row=0, column=1)
 
         button = tk.Button(self, text="Start Flashcards",
                            command=self.start_flash_cards)
@@ -41,6 +41,36 @@ class HomePage(tk.Frame):
         edit_flashcards = tk.Button(
             self, text="Edit Selected", command=self.on_top_level_button_click)
         edit_flashcards.grid(row=2, column=0)
+
+        new_flashcard_frame = tk.Frame(self)
+        new_flashcard_file_button = tk.Button(
+            new_flashcard_frame, text="Create New File", command=self.new_flashcard_file_button_clicked)
+        self.new_flashcard_entry = tk.Entry(new_flashcard_frame)
+        new_flashcard_file_button.pack(side=tk.RIGHT)
+        self.new_flashcard_entry.pack(side=tk.LEFT)
+        new_flashcard_frame.grid(row=3, column=0)
+
+    def new_flashcard_file_button_clicked(self):
+        filename = join(self.flashcard_file_folder,
+                        self.new_flashcard_entry.get() + ".flashcards")
+        current_files = listdir(self.flashcard_file_folder)
+        if filename in current_files:
+            self.set_entry_text(self.new_flashcard_entry,
+                                "FILE ALREADY EXISTS!!!")
+            return
+        with open(filename, 'w+') as f:
+            f.write(",,0")
+        self.set_entry_text(self.new_flashcard_entry, "Creation Successful!")
+
+        # Refreshing option menu
+        self.option_menu["menu"].delete(0, 'end')
+        for choice in listdir(self.flashcard_file_folder):
+            self.option_menu["menu"].add_command(
+                label=choice, command=tk._setit(self.selected_file_tkstring, choice))
+
+    def set_entry_text(self, entry_object, new_text):
+        entry_object.delete(0, tk.END)
+        entry_object.insert(0, new_text)
 
     def start_flash_cards(self):
         self.controller.show_frame("FlashcardPage")
