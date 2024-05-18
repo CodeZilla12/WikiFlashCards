26c599aad431e44890234f427547418aaa730f79
Sam-Gledhill <CodeZilla12@github.com>
2024-05-18 12:19:34 +0100

    Switched entry ->text widget

--- flashcard_viewer.py
+++ flashcard_viewer.py
@@ -27,12 +27,23 @@ class FlashcardViewer(tk.Toplevel):
         self.title("Flashcard Viewer")
 
         # Iterate through each entry in selected config.
+        entry_frame = tk.Frame(self)
 
-        e1 = tk.Entry(self)
-        e2 = tk.Entry(self)
+        # Using width to specify widget width means character length - not pixel length. Find a way for pixel width instead.
+        word_text_field = tk.Text(entry_frame, height=2, width=74)
 
-        e1.grid(row=0, column=0)
-        e2.grid(row=1, column=0)
+        # 1.0 refers to line 1 character 0
+        word_text_field.insert(
+            "1.0", self.word_trans_score_list[self.word_index][0])
+
+        trans_text_field = tk.Text(entry_frame, height=2, width=74)
+
+        trans_text_field.insert(
+            "1.0", self.word_trans_score_list[self.word_index][1])
+
+        word_text_field.grid(row=0, column=0)
+        trans_text_field.grid(row=1, column=0)
+        entry_frame.grid(row=0, column=0)
 
         button_frame = tk.Frame(self)
 
@@ -53,7 +64,7 @@ class FlashcardViewer(tk.Toplevel):
         next_button.grid(row=2, column=1)
         add_new_flashcard_button.grid(row=2, column=2)
         save_button.grid(row=2, column=3)
-        button_frame.grid(row=2, column=0)
+        button_frame.grid(row=2, column=0, sticky="s")
 
         self.mainloop()
 
