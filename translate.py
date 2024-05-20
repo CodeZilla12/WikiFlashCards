791c104d293e35cb4d20c33d1b63f9aa676dc5a7
Sam-Gledhill <CodeZilla12@github.com>
2024-05-20 14:29:31 +0100

    moved translation to separate python file

---
+++ translate.py
@@ -0,0 +1,59 @@
+import googletrans
+import time
+from tqdm import tqdm
+
+
+# Notes on translate api
+# The maximum character limit on a single text is 15k https://py-googletrans.readthedocs.io/en/latest/
+# Google Translate API also has a default limit of 2 million characters per day and 100,000 characters per 100 second https://help.stackby.com/article/71-google-translate
+# Can use googletrans.models to perhaps get confidence of translation
+
+
+TRANSLATE_CHAR_LIMIT = 15_000
+DELTA_CHAR = TRANSLATE_CHAR_LIMIT*0.1
+CHARS_PER_100_SECONDS = 100_000
+
+
+def translate_word_list(word_list):
+
+    total_chars = sum([len(i) for i in word_list])
+
+    translator = googletrans.Translator()
+
+    chunked_words = []
+    previous = 0
+    current_n_chars = 0
+    for i, word in enumerate(word_list):
+        current_n_chars += len(word)
+
+        if current_n_chars >= CHARS_PER_100_SECONDS:
+            # Indicate that this chunk should have a 100 second wait
+            chunked_words.append(["P"])
+
+        coefficient = len(chunked_words) if len(chunked_words) > 0 else 1
+        if current_n_chars >= (coefficient*TRANSLATE_CHAR_LIMIT) - DELTA_CHAR:
+            current_n_chars -= len(word)
+            chunked_words.append(word_list[previous:i-1])
+
+            previous = i
+
+    print(len(word_list))
+    # print(total_chars)
+    print(chunked_words)
+
+    print("Translating Words...")
+    time.sleep(1)
+    translated_words = []
+
+    for chunk in tqdm(chunked_words):
+        if not chunk:
+            continue
+        if chunk[0] == "P":
+            time.sleep(101)
+        # API is very slow. Multiple minutes.
+        translated_words.extend(
+            translator.translate(chunk, dest='en', src='lt'))
+
+    with open("Words.txt", "w+", encoding='utf-8') as f:
+        for lt, en in tqdm(zip(word_list, translated_words)):
+            f.write(f"{lt}:{en.text}\n")
--- wiki_scrape.py
+++ wiki_scrape.py
@@ -4,76 +4,26 @@
 # package words with english translation
 
 from words_from_wiki import get_words_from_articles
-
-from tqdm import tqdm
-import googletrans
-import time
+from translate import translate_word_list
 
 root = "https://lt.wikipedia.org"
 SEED_LINK = "https://lt.wikipedia.org/wiki/Taryb%C5%B3_S%C4%85junga"
 SEARCH_DEPTH = 1
 
-word_dict = get_words_from_articles(SEED_LINK, SEARCH_DEPTH)
 
-# https://stackoverflow.com/questions/12987178/sort-a-list-based-on-dictionary-values-in-python
-sorted_words = sorted(word_dict, key=word_dict.get)
+def grab_sorted_words(seed_link: str, search_depth: int):
+    """_summary_ Given a seed link an search depth, grab a list of words from an lt.wikipedia link 
 
-lt_alphabet = set("ertyuiopasdfghjklzxcvbnmąčęėįšųūž".upper()
-                  )  # missing key letters
-# filters out russian, greek alphabets etc.
-sorted_words = [i for i in sorted_words if set(i).issubset(lt_alphabet)]
+    Args:
+        seed_link (_type_):str _description_ First link which is searched for words and links
+        search_depth (_type_):int _description_ How many links deep do you need to go i.e 0:Don't follow any links, 1:Follow links from first page, 2:Follow links from 1 etc..
+    """
+    word_dict = get_words_from_articles(SEED_LINK, SEARCH_DEPTH)
 
-print(sorted_words)
+    # Sort words based on occurance in the links searched
+    sorted_words = sorted(word_dict, key=word_dict.get)
 
-quit()
-
-# Notes on translate api
-# The maximum character limit on a single text is 15k https://py-googletrans.readthedocs.io/en/latest/
-# Google Translate API also has a default limit of 2 million characters per day and 100,000 characters per 100 second https://help.stackby.com/article/71-google-translate
-# Can use googletrans.models to perhaps get confidence of translation
-
-TRANSLATE_CHAR_LIMIT = 15_000
-DELTA_CHAR = TRANSLATE_CHAR_LIMIT*0.1
-
-CHARS_PER_100_SECONDS = 100_000
-
-total_chars = sum([len(i) for i in sorted_words])
-
-translator = googletrans.Translator()
-
-chunked_words = []
-previous = 0
-current_n_chars = 0
-for i, word in enumerate(sorted_words):
-    current_n_chars += len(word)
-
-    if current_n_chars >= CHARS_PER_100_SECONDS:
-        # Indicate that this chunk should have a 100 second wait
-        chunked_words.append(["P"])
-
-    coefficient = len(chunked_words) if len(chunked_words) > 0 else 1
-    if current_n_chars >= (coefficient*TRANSLATE_CHAR_LIMIT) - DELTA_CHAR:
-        current_n_chars -= len(word)
-        chunked_words.append(sorted_words[previous:i-1])
-
-        previous = i
-
-print(len(sorted_words))
-# print(total_chars)
-print(chunked_words)
-
-print("Translating Words...")
-time.sleep(1)
-translated_words = []
-
-for chunk in tqdm(chunked_words):
-    if not chunk:
-        continue
-    if chunk[0] == "p":
-        time.sleep(101)
-    # API is very slow. Multiple minutes.
-    translated_words.extend(translator.translate(chunk, dest='en', src='lt'))
-
-with open("Words.txt", "w+", encoding='utf-8') as f:
-    for lt, en in tqdm(zip(sorted_words, translated_words)):
-        f.write(f"{lt}:{en.text}\n")
+    lt_alphabet = set("ertyuiopasdfghjklzxcvbnmąčęėįšųūž".upper()
+                      )  # missing key letters
+    # filters out russian, greek alphabets etc.
+    sorted_words = [i for i in sorted_words if set(i).issubset(lt_alphabet)]
