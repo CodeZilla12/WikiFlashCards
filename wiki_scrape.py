294e917a0856b16835b67273de0f34ad66920fff
Sam-Gledhill <CodeZilla12@github.com>
2024-05-20 15:15:39 +0100

    fixed typo

--- wiki_scrape.py
+++ wiki_scrape.py
@@ -17,7 +17,7 @@ def grab_sorted_words(seed_link: str, search_depth: int):
         seed_link (_type_):str _description_ First link which is searched for words and links
         search_depth (_type_):int _description_ How many links deep do you need to go i.e 0:Don't follow any links, 1:Follow links from first page, 2:Follow links from 1 etc..
     """
-    word_dict = get_words_from_articles(SEED_LINK, SEARCH_DEPTH)
+    word_dict = get_words_from_articles(seed_link, search_depth)
 
     # Sort words based on occurance in the links searched
     sorted_words = sorted(word_dict, key=word_dict.get)
